1a41941d006f7fb672b2f94f0659009cd204ed7e
Muhammad Hidayatullah <77865173+Muhammad-Hidayatullah@users.noreply.github.com>
2025-06-17 10:55:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,28 +47,32 @@ home_website_admin = st.Page(
     icon=":material/home:",
 )   
 
-
+halaman_dokter = st.Page(
+    page="pages_admin/2_dokter.py",
+    title="Dokter",
+    icon=":material/stethoscope:",
+)
 
 halaman_penyakit = st.Page(
-    page="pages_admin/2_penyakit.py",
+    page="pages_admin/3_penyakit.py",
     title="Penyakit",
     icon=":material/microbiology:",
 )
 
 halaman_gejala = st.Page(
-    page="pages_admin/3_gejala.py",
+    page="pages_admin/4_gejala.py",
     title="Gejala",
     icon=":material/symptoms:",
 )
 
 halaman_relasi_dan_gejala = st.Page(
-    page="pages_admin/4_relasi_penyakit_dan_gejala.py",
+    page="pages_admin/5_relasi_penyakit_dan_gejala.py",
     title="Relasi Penyakit dan Gejala",
     icon=":material/fact_check:",
 )
 
 halaman_artikel = st.Page(
-    page="pages_admin/5_artikel.py",
+    page="pages_admin/8_artikel.py",
     title="Artikel",
     icon=":material/article:",
 )
@@ -79,21 +83,39 @@ halaman_pengguna = st.Page(
     icon=":material/patient_list:",
 )
 
-halaman_riwayat_pengguna = st.Page(
-    page="pages_admin/7_riwayat_pengguna.py",
-    title="Riwayat Pengguna",
-    icon=":material/summarize:"
-)
+
 
 halaman_log_out = st.Page(
-    page="pages_admin/8_log_out.py",
+    page="pages_admin/9_log_out.py",
     title="Log Out",
     icon=":material/logout:"
 )
 
 
+home_website_dokter = st.Page(
+    page="pages_dokter/1_home_dokter.py",
+    title="Dokter",
+    icon=":material/endocrinology:",
+)  
+
+data_profil_dokter = st.Page(
+    page="pages_dokter/2_data_profil_dokter.py",
+    title="Profil Dokter",
+    icon=":material/account_circle:",
+) 
+
+halaman_riwayat_pengguna_dokter = st.Page(
+    page="pages_dokter/3_riwayat_pengguna.py",
+    title="Riwayat Pengguna",
+    icon=":material/medical_information:"
+)
 
 
+halaman_log_out_dokter = st.Page(
+    page="pages_dokter/4_logout.py",
+    title="Log Out",
+    icon=":material/logout:"
+)
 if "masuk_website" not in st.session_state:
     st.session_state.masuk_website = None
 
@@ -104,10 +126,13 @@ if st.session_state.masuk_website == None:
     st.session_state.pg.run()
     
 if st.session_state.masuk_website == "Admin":
-    st.session_state.pg = st.navigation(pages=[home_website_admin, halaman_penyakit, halaman_gejala, halaman_relasi_dan_gejala, 
-                                               halaman_pengguna, halaman_riwayat_pengguna, halaman_artikel, halaman_log_out])
+    st.session_state.pg = st.navigation(pages=[home_website_admin, halaman_dokter, halaman_penyakit, halaman_gejala, halaman_relasi_dan_gejala, 
+                                               halaman_pengguna, halaman_artikel, halaman_log_out])
     st.session_state.pg.run()
     
+if st.session_state.masuk_website == "Dokter":
+    st.session_state.pg = st.navigation(pages=[home_website_dokter, data_profil_dokter, halaman_riwayat_pengguna_dokter, halaman_log_out_dokter])
+    st.session_state.pg.run()
 
 
 
